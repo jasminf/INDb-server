15a7c63471b3d889a4bbfe2ef76f6e7a51a83acc
Jasmin <jasminf020@gmail.com>
2018-12-08 12:25:31 +0200

    Adding new password field to user migtation.

--- migrations/20181127165705-create-user.js
+++ migrations/20181127165705-create-user.js
@@ -20,6 +20,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.STRING
       },
+        password: {
+            allowNull: false,
+            type: Sequelize.STRING
+        },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- models/user.js
+++ models/user.js
@@ -6,9 +6,10 @@ module.exports = (sequelize, DataTypes) => {
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
+    password: DataTypes.STRING,
   }, {
     getterMethods   : {
-      fullName: function()  {
+      fullAsosiation: function()  {
         return this.firstName + ' ' + this.lastName;
       }
     },
